dea196aaab8e10e8d83fb8b4917320240dfec939
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-03 11:30:28 +0700

    Update Xfce Installer

---
+++ Xfce/Xfce-Installer-For-Termux.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update && apt upgrade -y
+apt install x11-repo
+apt install tigervnc '*xfce*' xterm -y
+
+vncserver
+export DISPLAY=":1"
+echo "Connect this IP address by VNC Viewer: 127.0.0.1:5901"
+
+xfce4-session
