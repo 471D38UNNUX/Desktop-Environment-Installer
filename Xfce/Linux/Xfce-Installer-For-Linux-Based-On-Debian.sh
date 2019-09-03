c527c92c7261765b680b73918f0397192db178a5
471D38UNNUX <52738122+471D38UNNUX@users.noreply.github.com>
2019-09-03 21:24:59 +0700

    Update Xfce Installer

--- Xfce/Linux/Xfce-Installer-For-Linux-Based-On-Debian.sh
+++ Xfce/Linux/Xfce-Installer-For-Linux-Based-On-Debian.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install desktop-base xfce4 xfce4-places-plugin xfce4-goodies xfce4-terminal synaptic firefox-esr
+sudo apt install desktop-base xfce4 xfce4-places-plugin xfce4-goodies xfce4-terminal xfe synaptic firefox-esr
